217c70bcfdd1f480aa5ab1b846aa630224b47fe7
pauloalarico <paulocraft20@gmail.com>
2026-02-17 23:57:46 -0300

    refactor - add validation for the dto of request.

--- src/main/java/org/order/orderrev/application/dto/request/NewOrderRequestDTO.java
+++ src/main/java/org/order/orderrev/application/dto/request/NewOrderRequestDTO.java
@@ -1,10 +1,19 @@
 package org.order.orderrev.application.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 import java.math.BigDecimal;
 
 public record NewOrderRequestDTO(
+        @NotBlank
         String product,
+        @NotNull
+        @Positive
         Integer quantity,
+        @Positive
+        @NotNull
         BigDecimal amount
 ) {
 }
