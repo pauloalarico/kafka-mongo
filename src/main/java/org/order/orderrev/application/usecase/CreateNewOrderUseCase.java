c8f36999c88e71008b37195e6fd5f081c879b3fa
pauloalarico <paulocraft20@gmail.com>
2026-02-19 01:09:44 -0300

    refactor - inject mapper via constructor and use the rich constructor of the entity

--- src/main/java/org/order/orderrev/application/usecase/CreateNewOrderUseCase.java
+++ src/main/java/org/order/orderrev/application/usecase/CreateNewOrderUseCase.java
@@ -3,37 +3,24 @@ package org.order.orderrev.application.usecase;
 import lombok.RequiredArgsConstructor;
 import org.order.orderrev.application.dto.request.NewOrderRequestDTO;
 import org.order.orderrev.application.dto.response.NewOrderResponseDTO;
-import org.order.orderrev.domain.entitie.Order;
-import org.order.orderrev.domain.enums.Status;
+import org.order.orderrev.domain.entity.Order;
 import org.order.orderrev.domain.repository.OrderRepository;
 import org.order.orderrev.infra.kafka.producer.OrderProducer;
 import org.order.orderrev.infra.mapper.OrderMapper;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.time.Instant;
-import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class CreateNewOrderUseCase {
     private final OrderProducer orderProducer;
     private final OrderRepository repository;
+    private final OrderMapper mapper;
 
-    @Transactional
     public NewOrderResponseDTO create(NewOrderRequestDTO dto) {
-        var order = Order.builder()
-                .id(UUID.randomUUID().toString())
-                .correlationId(UUID.randomUUID().toString())
-                .product(dto.product())
-                .quantity(dto.quantity())
-                .amount(dto.amount())
-                .status(Status.CREATED)
-                .createdAt(Instant.now())
-                .build();
+        var order = Order.create(dto.product(), dto.amount(), dto.quantity());
 
         repository.save(order);
-        var orderDto = OrderMapper.toResponseDto(order);
+        var orderDto = mapper.toResponseDto(order);
         orderProducer.sendOrder(orderDto);
         return orderDto;
     }
