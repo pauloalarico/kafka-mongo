8add87cc782e8617a295ab8de6d5ff720c55086e
pauloalarico <paulocraft20@gmail.com>
2026-02-19 01:08:19 -0300

    refactor - add resistance for the failures and poison pills for kafka

--- src/main/java/org/order/orderrev/infra/kafka/producer/OrderProducer.java
+++ src/main/java/org/order/orderrev/infra/kafka/producer/OrderProducer.java
@@ -3,25 +3,31 @@ package org.order.orderrev.infra.kafka.producer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.order.orderrev.application.dto.response.NewOrderResponseDTO;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.ExecutionException;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class OrderProducer {
     private final KafkaTemplate<String, Object> kafkaTemplate;
+    @Value("${apps.order-create-topic}")
+    private String orderTopic;
 
     public void sendOrder(NewOrderResponseDTO dto) {
-        kafkaTemplate.send("order-topic", dto.orderId(), dto)
-                .whenComplete((success, e) -> {
-                    if (e != null) {
-                        log.warn("Could not send the order, {}", e.getMessage());
-                    }
-                    else {
-                        log.info("Order sent: id {}, correlation id: {}", dto.orderId(), dto.correlationId());
-                    }
-        });
+        try {
+            kafkaTemplate.send(orderTopic, dto.orderId(), dto).get();
+        } catch (ExecutionException e) {
+            log.error("Unable to send message, error: {}", e.getMessage());
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            log.error("Thread interrupt while sending message, error: {}", e.getMessage());
+            throw new RuntimeException("Interrupted Exception: " + e.getMessage());
+        }
 
     }
 }
